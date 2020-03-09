333db8ff04c0a8f8c2b3dc730b7b3e74ae4f27af
socratone <gim2origin@gmail.com>
2020-03-10 01:20:19 +0900

    add Paint Line

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ canvas.height = 500;
 ctx.strokeStyle = 'black';
 ctx.lineWidth = 1;
 
+// 원점 그리기
+ctx.beginPath();
+ctx.arc(250, 250, 1, 0, 2 * Math.PI);
+ctx.stroke();
+
 // numInput을 클릭했을 때 글자가 없어진다.
 const numInput = document.getElementById('numInput');
 let hasClickNumInput = false;
@@ -18,16 +23,68 @@ function clickNumInput() {
 }
 numInput.addEventListener('click', clickNumInput);
 
-// 원점
-ctx.beginPath();
-ctx.arc(250, 250, 1, 0, 2 * Math.PI);
-ctx.stroke();
+function paint90Line() {
+  // 세로선
+  ctx.beginPath();
+  ctx.moveTo(250, 0);
+  ctx.lineTo(250, 500);
+  ctx.stroke();
+}
 
-// // 세로선
-// ctx.beginPath();
-// ctx.moveTo(250, 0);
-// ctx.lineTo(250, 500);
-// ctx.stroke();
+function paint180Line() {
+  ctx.beginPath();
+  ctx.moveTo(0, 250);
+  ctx.lineTo(500, 250);
+  ctx.stroke();
+}
+
+function paint45Line() {
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.lineTo(500, 500);
+  ctx.stroke();
+}
+
+function paint135Line() {
+  ctx.beginPath();
+  ctx.moveTo(0, 500);
+  ctx.lineTo(500, 0);
+  ctx.stroke();
+}
+
+// 클릭 버튼을 클릭했을 때 (숫자 얻어오기)
+const numButton = document.getElementById('numButton');
+function clickNumButton() {
+  const num = Number(numInput.value);
+  if(num) {
+    if(num === 2) {
+      paint90Line();
+      numInput.remove();
+      numButton.remove();
+    } else if(num === 4) {
+      paint90Line();
+      paint180Line();
+      numInput.remove();
+      numButton.remove();
+    } else if(num === 8) {
+      paint90Line();
+      paint180Line();
+      paint45Line();
+      paint135Line();
+      numInput.remove();
+      numButton.remove();
+    } else {
+      alert('2, 4, 8 중의 숫자를 입력해주세요.')
+    }
+  } else {
+    alert('숫자를 입력해주세요.');
+  } 
+}
+numButton.addEventListener('click', clickNumButton);
+
+
+
+
 
 // // 연습선
 // ctx.beginPath();
